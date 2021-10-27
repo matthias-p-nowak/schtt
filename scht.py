4cf93bb36572141af67324543174f5b9d343ec8f
matthias-p-nowak <matth-no@online.no>
2021-10-27 21:30:01 +0200

    added thread pool executors

--- scht.py
+++ scht.py
@@ -4,21 +4,27 @@ import logging
 import jinja2
 import yaml
 import sys
+import concurrent.futures.thread
 
 cfg = {}
 defaultCFG = """
 ---
 endpoint:
+  - name: rhino
     host: localhost
     port: 5060
+concurrent: 5
+tests:
+  tests:
 """
-
+ttpe: concurrent.futures.thread.ThreadPoolExecutor = None
+itpe: concurrent.futures.thread.ThreadPoolExecutor = None
 
 def main():
     """
     Use scht.py <config file>
     """
-    global cfg
+    global cfg,ttpe,itpe
     if len(sys.argv) < 2:
         print(main.__doc__)
         sys.exit(2)
@@ -28,6 +34,12 @@ def main():
         cfg2=yaml.safe_load(cf)
         if cfg2 is not None:
             cfg.update(yaml.safe_load(cf))
+    # a threadpoolexecutor for the infrastructure
+    itpe=concurrent.futures.thread.ThreadPoolExecutor()
+    # only for the tests
+    with concurrent.futures.thread.ThreadPoolExecutor() as exec:
+        ttpe=exec
+
     logging.debug("ended")
 
 if __name__ == '__main__':
